cade47c894e92d19ff8e49f127c355bd75a69c74
Solomon <sholasolomon@yahoo.com>
2023-12-22 19:32:08 +0000

    About page

--- src/components/GetUpdates/Update.jsx
+++ src/components/GetUpdates/Update.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import './update.css'
+import {Container, Col, Row} from 'reactstrap'
+import updateImg from '../../assets/getUpdates_img.png'
 
 function Update() {
   return (
-    <div>Update</div>
+    <section>
+      <Container>
+        <Row>
+          <Col lg='6' md='6' className='text-left'>
+            <div className="update-content">
+              <div className="text-white pb-4">
+                <h2 className="update-title text-white">
+                  Get Updates
+                </h2>
+                <p className="update-p text-white">
+                  Join Our Newsletter for the latest updates
+                </p>
+              </div>
+              <div className="getUpdates">
+                <input type="text" placeholder='email@example.com' />
+                <button className="btn btn-update">Subscribe</button>
+              </div>
+            </div>
+          </Col>
+          <Col lg='6' md='6'>
+              <div className="update-img">
+                <img src={updateImg} alt="update" className='w-100'/>
+              </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import './about.css'
+import {Container, Row, Col} from 'reactstrap'
+import aboutImg from '../../assets/aboutUs_img.png'
 
 function About() {
   return (
-    <div>About</div>
+    <section>
+      <Container>
+        <Row>
+          <Col lg='6' md='6'>
+            <div className="about-content">
+              <h2>About Us</h2>
+              <p>Rapt 'N Rel is a programming learning
+                platform developed by Nuevicsu Tech with the aim of empowering individuals to become proficient programmers.
+This platform offers comprehensive and user-friendly tutorials to cater to people of varying levels of expertise
+              </p>
+            </div>
+          </Col>
+
+          <Col lg='6' md='6'>
+            <div className="about_img">
+              <img src={aboutImg} alt="about us" className='w-100'/>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
